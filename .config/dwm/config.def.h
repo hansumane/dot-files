ee1293d7364d4e8d5943f34a948b85bb4e2534fe
Creasure <yurxo@bk.ru>
2022-01-06 00:38:35 +0300

    some changes v0.3

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -3,7 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 1;                                /* border pixel of windows */
 static const unsigned int snap      = 24;                               /* snap pixel */
-// static const unsigned int gappx     = 6;                                /* gaps between windows */
 static const int showbar            = 1;                                /* 0 means no bar */
 static const int topbar             = 1;                                /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrains Mono:size=13" };
