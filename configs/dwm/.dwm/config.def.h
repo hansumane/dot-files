08e77beba05c16616c20fff2ac3089316d475d7b
Creasure <yurxo@bk.ru>
2022-01-14 02:15:56 +0300

    dwm windows customization

--- configs/dwm/.dwm/config.def.h
+++ configs/dwm/.dwm/config.def.h
@@ -25,7 +25,8 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Pcmanfm",  NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Zathura",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
