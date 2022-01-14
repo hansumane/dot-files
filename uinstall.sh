c17361277c8481545fc32d77ea50a0a3210ba6fd
Creasure <yurxo@bk.ru>
2022-01-15 01:36:26 +0300

    uinstall.sh fix

--- uinstall.sh
+++ uinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh;
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 cd ~/.oh-my-zsh/custom/plugins;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git;
 git clone https://github.com/zsh-users/zsh-autosuggestions.git;
