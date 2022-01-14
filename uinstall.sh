46c03ebbd1db8d04214e5a59313c408771b673af
Creasure <yurxo@bk.ru>
2022-01-14 23:25:00 +0300

    uinstall huge purification

--- uinstall.sh
+++ uinstall.sh
@@ -1,21 +1,31 @@
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-cd ~/.oh-my-zsh/custom/plugins &&
+#!/bin/sh
+
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh;
+cd ~/.oh-my-zsh/custom/plugins;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git;
-# I'm not using zsh-autosuggestions temporarily...
-# git clone https://github.com/zsh-users/zsh-autosuggestions.git;
-cd; git clone https://github.com/sindresorhus/pure.git &&
-cd pure &&
-cat pure.zsh > ~/.oh-my-zsh/custom/themes/pure.zsh-theme &&
-mkdir ~/.oh-my-zsh/functions &&
-cat async.zsh > ~/.oh-my-zsh/functions/async &&
+git clone https://github.com/zsh-users/zsh-autosuggestions.git;
+
+cd; git clone https://github.com/sindresorhus/pure.git;
+cd pure;
+cat pure.zsh > ~/.oh-my-zsh/custom/themes/pure.zsh-theme;
+mkdir ~/.oh-my-zsh/functions;
+cat async.zsh > ~/.oh-my-zsh/functions/async;
 cd && rm -rf pure;
-cp ~/dot-files/zshthemes/* ~/.oh-my-zsh/custom/themes &&
+
+cp ~/dot-files/zshthemes/* ~/.oh-my-zsh/custom/themes;
 rm -rf ~/.oh-my-zsh/custom/themes/example.zsh-theme;
-mkdir ~/.config &&
-cp -rf ~/dot-files/configs/universal/.config/nvim ~/.config &&
+
+cd; if [ ! -d "$(pwd)/.config" ]; then mkdir ~/.config; fi;
+
+cp -rf ~/dot-files/configs/universal/.config/nvim ~/.config;
 cp -rf ~/dot-files/configs/universal/.zshrc ~;
-cd; mkdir Desktop Downloads Others && cd Others &&
-mkdir -p Templates Shared Documents Music Pictures/Screenshots Videos &&
+
+cd; if [ ! -d "$(pwd)/Desktop" ]; then mkdir Desktop; fi;
+if [ ! -d "$(pwd)/Downloads" ]; then mkdir Downloads; fi;
+if [ ! -d "$(pwd)/Others" ]; then mkdir Others; fi;
+
+cd ~/Others;
+mkdir -p Templates Shared Documents Music Pictures/Screenshots Videos;
 echo 'XDG_DESKTOP_DIR="$HOME/Desktop"
 XDG_DOWNLOAD_DIR="$HOME/Downloads"
 XDG_TEMPLATES_DIR="$HOME/Others/Templates"
@@ -23,5 +33,5 @@ XDG_PUBLICSHARE_DIR="$HOME/Others/Shared"
 XDG_DOCUMENTS_DIR="$HOME/Others/Documents"
 XDG_MUSIC_DIR="$HOME/Others/Music"
 XDG_PICTURES_DIR="$HOME/Others/Pictures"
-XDG_VIDEOS_DIR="$HOME/Others/Videos"' > ~/.config/user-dirs.dirs &&
-echo 'en_US' > ~/.config/user-dirs.locale
+XDG_VIDEOS_DIR="$HOME/Others/Videos"' > ~/.config/user-dirs.dirs;
+echo 'en_US' > ~/.config/user-dirs.locale;
