d5dac4db2cb3746a3942e53b8f2bd00e964d8ba3
Creasure <yurxo@bk.ru>
2022-01-27 04:21:30 +0300

    added termux.properties

--- .termux/tinstall.sh
+++ .termux/tinstall.sh
@@ -1,7 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
-echo "bell-character = ignore" >> ~/.termux/termux.properties;
-
 pkg upgrade -y;
 apt update;
 apt full-upgrade -y;
@@ -13,7 +11,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git;
 git clone https://github.com/zsh-users/zsh-autosuggestions.git;
 
 cp ~/storage/dot-files/.termux/font.ttf ~/.termux;
-cp ~/storage/dot-files/.termux/colors.properties ~/.termux;
+cp ~/storage/dot-files/.termux/*.properties ~/.termux;
 cp ~/storage/dot-files/zshthemes/* ~/.oh-my-zsh/custom/themes;
 rm ~/.oh-my-zsh/custom/themes/example.zsh-theme;
 
