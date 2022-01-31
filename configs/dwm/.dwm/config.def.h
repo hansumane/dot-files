306110277d8fa222c4a22e3b6c5bf7407dc1f507
Creasure <yurxo@bk.ru>
2022-01-31 10:08:01 +0300

    add dwm patches

--- configs/dwm/.dwm/config.def.h
+++ configs/dwm/.dwm/config.def.h
@@ -2,6 +2,13 @@
 static const unsigned int borderpx  = 1;            /* border pixel of windows */
 static const unsigned int snap      = 24;           /* shows how many pixels to stick
                                                        a window to screen border etc */
+
+static const unsigned int gappih    = 10;           /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;           /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;           /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;           /* vert outer gap between windows and screen edge */
+static int smartgaps                = 1;            /* 1 means no outer gap when there is only one window */
+
 static const int showbar            = 1;            /* 0 means no bar */
 static const int topbar             = 1;            /* 0 means bottom bar */
 static const char *fonts[]          = { "VictorMono Nerd Font:size=13:style=SemiBold:antialias=true:autohint=true" };
@@ -19,7 +26,6 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-/* I prefer to see tags as numbers and not icons */
 static const char *tags[] = { "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
@@ -35,11 +41,26 @@ static const int nmaster        = 1;    /* number of clients in master area */
 static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile },
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -73,12 +94,27 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ControlMask,           XK_u,      incrgaps,       {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask, XK_u,      incrgaps,       {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_i,      incrigaps,      {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask, XK_i,      incrigaps,      {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_o,      incrogaps,      {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask, XK_o,      incrogaps,      {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_6,      incrihgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask, XK_6,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_7,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask, XK_7,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_8,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask, XK_8,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_9,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask, XK_9,      incrovgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_0,      togglegaps,     {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	// { MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[13]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
