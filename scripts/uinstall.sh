b209f58e0cb51197e6ba132870e231e6a8251de4
Creasure <yurxo@bk.ru>
2022-04-17 23:01:04 +0300

    updated ohmyzsh config and uinstall.sh

--- scripts/uinstall.sh
+++ scripts/uinstall.sh
@@ -1,19 +1,17 @@
-#!/usr/bin/bash
-set -e
+#!/bin/sh
 
-cd;
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+cd; sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+if [ ! -d ~/.oh-my-zsh/custom/plugins ]; then mkdir ~/.oh-my-zsh/custom/plugins; fi;
 cd ~/.oh-my-zsh/custom/plugins;
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git;
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git;
 
-cd;
-rm ~/.oh-my-zsh/custom/themes/example.zsh-theme;
+rm ~/.oh-my-zsh/custom/themes/*;
 cp ~/dot-files/themes/zsh_themes/* ~/.oh-my-zsh/custom/themes;
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
-cd; if [ ! -d "$(pwd)/.config" ]; then mkdir ~/.config; fi;
-# if [ -d "$(pwd)/.config/kitty" ]; then rm -rf ~/.config/kitty; fi;
+if [ ! -d ~/.config ]; then mkdir ~/.config; fi;
+# if [ -d ~/.config/kitty ]; then rm -r ~/.config/kitty; fi;
 
 cp -r ~/dot-files/configs/universal/.config/nvim ~/.config;
 # cp -r ~/dot-files/configs/universal/.config/kitty ~/.config;
@@ -21,13 +19,13 @@ cp -r ~/dot-files/configs/universal/.config/nvim ~/.config;
 cp ~/dot-files/configs/universal/.zshrc ~;
 cp ~/dot-files/configs/universal/.p10k.zsh ~;
 
-cd; if [ ! -d "$(pwd)/Desktop" ]; then mkdir Desktop; fi;
-if [ ! -d "$(pwd)/Downloads" ]; then mkdir Downloads; fi;
-if [ -d "$(pwd)/Others" ]; then mv Others Others_old; fi;
-mkdir Others;
+if [ ! -d ~/Desktop ]; then mkdir ~/Desktop; fi;
+if [ ! -d ~/Downloads ]; then mkdir ~/Downloads; fi;
+if [ -d ~/Others ]; then mv ~/Others ~/Others_old; fi;
+mkdir ~/Others;
 
 cd ~/Others;
-mkdir -p Coding Documents Music Pictures/Screenshots Shared Templates Videos;
+mkdir -p etc Coding Documents Music Pictures/Screenshots Shared Templates Videos;
 echo 'XDG_DESKTOP_DIR="$HOME/Desktop"
 XDG_DOWNLOAD_DIR="$HOME/Downloads"
 XDG_TEMPLATES_DIR="$HOME/Others/Templates"
@@ -38,4 +36,4 @@ XDG_PICTURES_DIR="$HOME/Others/Pictures"
 XDG_VIDEOS_DIR="$HOME/Others/Videos"' > ~/.config/user-dirs.dirs;
 
 cd ~/.config/nvim;
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
+sh -c 'curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
