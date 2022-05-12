005877a9d4bdc65db2506cca72d48e081ce62b42
Creasure <yurxo@bk.ru>
2022-05-12 19:51:50 +0300

    updated autofontpatcher.sh script and added mononoki nerd font

--- scripts/autofontpatcher.sh
+++ scripts/autofontpatcher.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+set -e
 
-FONTNAME="JetBrainsMono"
+FONTNAME="mononoki"
+EXTENSION=".ttf"
+DIRNAME="$(pwd)"
 
-for FONT in ~/Downloads/${FONTNAME}All/*.EXTENSION;
-do
-    ~/Downloads/nerd-fonts/font-patcher \
+if [ ! -d ${DIRNAME}/nerd-fonts ]; then
+    git clone --depth=1 --recursive https://github.com/ryanoasis/nerd-fonts.git;
+fi;
+
+if [ ! -d ${DIRNAME}/${FONTNAME}All ]; then
+    echo "No such font directory: ${FONTNAME}All";
+    exit;
+else
+    mkdir -p ${DIRNAME}/${FONTNAME}All/${FONTNAME}Patched;
+fi;
+
+for FONT in ${DIRNAME}/${FONTNAME}All/*${EXTENSION}; do
+    ${DIRNAME}/nerd-fonts/font-patcher \
     $FONT --complete --progressbars --outputdir \
-    ~/Downloads/${FONTNAME}Patched;
+    ${DIRNAME}/${FONTNAME}All/${FONTNAME}Patched;
 done;
