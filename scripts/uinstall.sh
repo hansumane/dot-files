db400999c6ea3e8f3979820d388e92ef28986fae
Creasure <yurxo@bk.ru>
2022-05-12 14:42:05 +0300

    purified uinstall.sh script

--- scripts/uinstall.sh
+++ scripts/uinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd; sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-if [ ! -d ~/.oh-my-zsh/custom/plugins ]; then mkdir ~/.oh-my-zsh/custom/plugins; fi;
+mkdir -p ~/.oh-my-zsh/custom/plugins;
 cd ~/.oh-my-zsh/custom/plugins;
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git;
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git;
@@ -9,20 +9,16 @@ git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git;
 cp ~/dot-files/themes/zsh_themes/* ~/.oh-my-zsh/custom/themes;
 # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
-if [ ! -d ~/.config ]; then mkdir ~/.config; fi;
+mkdir -p ~/.config
 
 cp -r ~/dot-files/configs/universal/.gitconfig ~;
 cp -r ~/dot-files/configs/universal/.config/nvim ~/.config;
 cp ~/dot-files/configs/universal/.zshrc ~;
 # cp ~/dot-files/configs/universal/.p10k.zsh ~/.oh-my-zsh;
 
-if [ ! -d ~/Desktop ]; then mkdir ~/Desktop; fi;
-if [ ! -d ~/Downloads ]; then mkdir ~/Downloads; fi;
-if [ -d ~/Others ]; then mv ~/Others ~/Others_old; fi;
-mkdir ~/Others;
-
-cd ~/Others;
-mkdir -p etc Coding Documents Music Pictures/Screenshots Shared Templates Videos;
+cd; mkdir -p Desktop Downloads Others;
+cd Others;
+mkdir -p etc Coding Documents Music Pictures Shared Templates Videos;
 echo 'XDG_DESKTOP_DIR="$HOME/Desktop"
 XDG_DOWNLOAD_DIR="$HOME/Downloads"
 XDG_TEMPLATES_DIR="$HOME/Others/Templates"
