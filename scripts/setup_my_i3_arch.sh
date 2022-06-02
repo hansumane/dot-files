394057a81c4457f36b382bb3af9c58c221df4f69
Creasure <yurxo@bk.ru>
2022-06-03 00:13:25 +0300

    updated setup_my_i3_arch.sh

--- scripts/setup_my_i3_arch.sh
+++ scripts/setup_my_i3_arch.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e;
 
-sudo pacman -S --needed \
+mkdir -p ~/Downloads;
+cd ~/Downloads;
+git clone --depth=1 --recusrsive https://aur.archlinux.org/yay.git;
+cd ~/Downloads/yay;
+makepkg -sic;
+cd; rm -rf ~/Downloads/yay;
+
+sudo pacman -Syy --needed \
   i3-gaps i3lock i3blocks rofi kitty xcompmgr feh sxiv \
   xfce4-clipman-plugin xfce4-screenshooter lxappearance pavucontrol \
   network-manager-applet networkmanager-openvpn \
@@ -9,26 +16,22 @@ sudo pacman -S --needed \
   flatpak firefox firefox-i18n-ru firefox-ublock-origin \
   zathura zathura-pdf-mupdf zathura-djvu;
 
-cp -f       ~/dot-files/configs/i3/.xinitrc ~;
-cp -rf      ~/dot-files/configs/i3/.config/i3 ~/.config;
-cp -rf      ~/dot-files/configs/i3/.config/i3blocks ~/.config;
-cp -rf      ~/dot-files/configs/i3/.config/rofi ~/.config;
-cp -f       ~/dot-files/configs/universal/.Xdefaults ~;
-cp -rf      ~/dot-files/configs/universal/.config/kitty ~/.config;
-cp -rf      ~/dot-files/configs/universal/.config/neofetch ~/.config;
+yay -Syy --needed hunspell hunspell-en_us hunspell-ru \
+                  hyphen hyphen-en hyphen-ru;
 
-sudo mkdir -p /etc/X11/xorg.conf.d;
-sudo cp -f  ~/dot-files/X11/xorg.conf.d/00-keyboard.conf /etc/X11/xorg.conf.d;
-sudo cp -rf ~/dot-files/themes/gtk_themes/Everforest_Dark /usr/share/themes;
-sudo cp -rf ~/dot-files/themes/icon_themes/Zafiro-Icons-Dark /usr/share/icons;
-sudo cp -rf ~/dot-files/themes/icon_themes/Simp1e-Gruvbox-* /usr/share/icons;
+cp -f   ~/dot-files/configs/i3/.xinitrc ~;
+cp -rf  ~/dot-files/configs/i3/.config/i3 ~/.config;
+cp -rf  ~/dot-files/configs/i3/.config/i3blocks ~/.config;
+cp -rf  ~/dot-files/configs/i3/.config/rofi ~/.config;
+cp -f   ~/dot-files/configs/universal/.Xdefaults ~;
+cp -rf  ~/dot-files/configs/universal/.config/kitty ~/.config;
+cp -rf  ~/dot-files/configs/universal/.config/neofetch ~/.config;
 
-mkdir -p ~/Downloads;
-cd ~/Downloads;
-git clone --depth=1 https://aur.archlinux.org/yay.git;
-cd ~/Downloads/yay;
-rm -rf .git*;
-makepkg -sic;
+sudo mkdir -p  /etc/X11/xorg.conf.d;
+sudo cp -f     ~/dot-files/X11/xorg.conf.d/00-keyboard.conf /etc/X11/xorg.conf.d;
+sudo cp -rf    ~/dot-files/themes/gtk_themes/Everforest_Dark /usr/share/themes;
+sudo cp -rf    ~/dot-files/themes/icon_themes/Zafiro-Icons-Dark /usr/share/icons;
+sudo cp -rf    ~/dot-files/themes/icon_themes/Simp1e-Gruvbox-* /usr/share/icons;
 
 cd ~/Downloads;
 git clone --depth=1 --recursive https://github.com/nonpop/xkblayout-state.git;
