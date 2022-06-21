cd3f66e6d4035f88c54cd2c517d8b8dec26f4002
Creasure <yurxo@bk.ru>
2022-06-21 19:58:18 +0300

    moved test to z_os-release_name.sh

--- scripts/setup/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e;
-
-sudo sed 's/Exec=.*/#Exec=\/usr\/bin\/kwalletd5/g' -i /usr/share/dbus-1/services/org.kde.kwalletd5.service;
---
+++ scripts/setup/z_os-release_name.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e;
+
+source /etc/os-release;
+echo $NAME;
