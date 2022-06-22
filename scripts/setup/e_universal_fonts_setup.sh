88a650e21df40486365bac845a03a6dd0ca6cd6d
Creasure <yurxo@bk.ru>
2022-06-22 18:58:18 +0300

    edited auto font setup script

--- scripts/setup/e_universal_fonts_setup.sh
+++ scripts/setup/e_universal_fonts_setup.sh
@@ -11,10 +11,14 @@ fi;
 echo "you'll need to grant root access to copy fonts to /usr/share/fonts/ManuallyInstalled";
 
 cd $(pwd)/../../.fonts;
+sudo rm -rf /usr/share/fonts/ManuallyInstalled;
 sudo mkdir -p /usr/share/fonts/ManuallyInstalled;
+  # sudo tar -xf CascadiaCodeAll.txz -C /usr/share/fonts/ManuallyInstalled;
+  sudo tar -xf FiraCodeAll.txz -C /usr/share/fonts/ManuallyInstalled;
   sudo tar -xf HackAll.txz -C /usr/share/fonts/ManuallyInstalled;
-  sudo tar -xf IosevkaAll.txz -C /usr/share/fonts/ManuallyInstalled;
-  sudo tar -xf IosevkaTermAll.txz -C /usr/share/fonts/ManuallyInstalled;
+  # sudo tar -xf IosevkaAll.txz -C /usr/share/fonts/ManuallyInstalled;
+  # sudo tar -xf IosevkaTermAll.txz -C /usr/share/fonts/ManuallyInstalled;
   sudo tar -xf JetBrainsMonoAll.txz -C /usr/share/fonts/ManuallyInstalled;
+  # sudo tar -xf VictorMonoAll.txz -C /usr/share/fonts/ManuallyInstalled;
 sudo chown root:root -R /usr/share/fonts/ManuallyInstalled;
 fc-cache -r;
