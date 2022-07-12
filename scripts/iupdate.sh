6050364ac09cb367c7002829a27f9c05c7e1038d
Creasure <yurxo@bk.ru>
2022-07-12 17:07:23 +0300

    updated setup scripts

--- scripts/iupdate.sh
+++ scripts/iupdate.sh
@@ -3,24 +3,30 @@ set -e;
 
 if [ ! $(pwd | rev | cut -d'/' -f2 | rev) = 'dot-files' ] ||
     [ ! $(pwd | rev | cut -d'/' -f1 | rev) = 'scripts' ]; then
-  echo "please go to ?/dot-files/scripts folder and run script from there!";
+  echo "please go to /scripts folder and run script from there!";
   exit 1;
 else
   cd $(git rev-parse --show-toplevel); CURRENT_DIR=$(pwd);
 fi;
 
-echo "> /fonts <";
+echo "-> /fonts ...";
 if [ ! -d ${CURRENT_DIR}/fonts ]; then
-  git clone https://github.com/hansumane/fonts.git;
+  git clone https://github.com/hansumane/fonts.git > /dev/null;
+  echo "---> /fonts cloned.";
 else
   cd ${CURRENT_DIR}/fonts;
-  git pull --rebase;
+  git pull --rebase > /dev/null;
+  echo "---> /fonts updated.";
 fi;
 
-echo "> /wallpapers <";
+echo "-> /wallpapers ...";
 if [ ! -d ${CURRENT_DIR}/wallpapers ]; then
-  git clone https://github.com/hansumane/wallpapers.git;
+  git clone https://github.com/hansumane/wallpapers.git > /dev/null;
+  echo "---> /wallpapers cloned.";
 else
   cd ${CURRENT_DIR}/wallpapers;
-  git pull --rebase;
+  git pull --rebase > /dev/null;
+  echo "---> /wallpapers updated.";
 fi;
+
+echo "-> done!";
--- scripts/setup/others/b_rocky_default.sh
+++ scripts/setup/others/b_rocky_default.sh
@@ -5,7 +5,7 @@ if [ ! $(pwd | rev | cut -d'/' -f4 | rev) = 'dot-files' ] ||
     [ ! $(pwd | rev | cut -d'/' -f3 | rev) = 'scripts' ] ||
     [ ! $(pwd | rev | cut -d'/' -f2 | rev) = 'setup' ] ||
     [ ! $(pwd | rev | cut -d'/' -f1 | rev) = 'others' ]; then
-  echo "please go to ?/dot-files/scripts/setup folder and run script from there!";
+  echo "please go to /scripts/setup/others folder and run script from there!";
   exit 1;
 else
   cd $(git rev-parse --show-toplevel); CURRENT_DIR=$(pwd);
