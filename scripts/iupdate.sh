08ae5ce89a2e0824db34ce6ae61337817826eb4c
Creasure <yurxo@bk.ru>
2022-07-12 16:06:00 +0300

    placed /fonts and /wallpapers into another repos and added script to update

---
+++ scripts/iupdate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e;
+
+if [ ! $(pwd | rev | cut -d'/' -f2 | rev) = 'dot-files' ] ||
+    [ ! $(pwd | rev | cut -d'/' -f1 | rev) = 'scripts' ]; then
+  echo "please go to ?/dot-files/scripts folder and run script from there!";
+  exit 1;
+else
+  cd $(git rev-parse --show-toplevel); CURRENT_DIR=$(pwd);
+fi;
+
+if [ ! -d ${CURRENT_DIR}/fonts ]; then
+  git clone https://github.com/hansumane/fonts.git;
+else
+  cd ${CURRENT_DIR}/fonts;
+  git pull --rebase;
+fi;
+
+if [ ! -d ${CURRENT_DIR}/wallpapers ]; then
+  git clone https://github.com/hansumane/wallpapers.git;
+else
+  cd ${CURRENT_DIR}/wallpapers;
+  git pull --rebase;
+fi;
