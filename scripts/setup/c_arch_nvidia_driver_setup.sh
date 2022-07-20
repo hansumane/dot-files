9cdf4ae834709cd1d7f0c4532be9d91b0d51016b
Creasure <yurxo@bk.ru>
2022-07-20 18:27:29 +0300

    removed loglevel from arch setup script

--- scripts/setup/c_arch_nvidia_driver_setup.sh
+++ scripts/setup/c_arch_nvidia_driver_setup.sh
@@ -4,7 +4,7 @@ set -e;
 sudo pacman -S --needed --noconfirm \
   mesa nvidia-open-dkms nvidia-utils opencl-nvidia;
 
-sudo sed 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash loglevel=3 nvidia nvidia_modeset nvidia_uvm nvidia_drm nvidia-drm.modeset=1"/g' -i /etc/default/grub;
+sudo sed 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nvidia nvidia_modeset nvidia_uvm nvidia_drm nvidia-drm.modeset=1"/g' -i /etc/default/grub;
 
 sudo mkinitcpio -P;
 sudo update-grub;
