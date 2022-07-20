fe73b6dfc59296249a36b8ccec06f84d5eb8ad48
Creasure <yurxo@bk.ru>
2022-07-21 01:59:42 +0300

    removed useless git status from iupdate.sh

--- scripts/iupdate.sh
+++ scripts/iupdate.sh
@@ -15,13 +15,11 @@ if [ ! -d ${CURRENT_DIR}/fonts ]; then
   echo "---> cloning /fonts ...";
   git clone --depth=1 https://github.com/hansumane/fonts.git &> /dev/null;
   cd ${CURRENT_DIR}/fonts;
-  git status &> /dev/null;
   echo "---> /fonts cloned.";
 else
   cd ${CURRENT_DIR}/fonts;
   echo "---> pulling /fonts ...";
   git pull --rebase &> /dev/null;
-  git status &> /dev/null;
   echo "---> /fonts updated.";
 fi;
 
@@ -31,13 +29,11 @@ if [ ! -d ${CURRENT_DIR}/wallpapers ]; then
   echo "---> cloning /wallpapers ...";
   git clone --depth=1 https://github.com/hansumane/wallpapers.git &> /dev/null;
   cd ${CURRENT_DIR}/wallpapers;
-  git status &> /dev/null;
   echo "---> /wallpapers cloned.";
 else
   cd ${CURRENT_DIR}/wallpapers;
   echo "---> pulling /wallpapers ...";
   git pull --rebase &> /dev/null;
-  git status &> /dev/null;
   echo "---> /wallpapers updated.";
 fi;
 
