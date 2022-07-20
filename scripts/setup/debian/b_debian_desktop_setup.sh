3c46e253ccbca11f3415d6e44fb8387df74b8b92
Creasure <yurxo@bk.ru>
2022-07-20 18:26:45 +0300

    updated debian setup script

--- scripts/setup/debian/b_debian_desktop_setup.sh
+++ scripts/setup/debian/b_debian_desktop_setup.sh
@@ -22,16 +22,20 @@ else
   echo "your CPU is neither Intel nor AMD";
 fi;
 
+EXA_VERSION=0.10.1
+HEXYL_VERSION=0.10.0
+BAT_VERSION=0.21.0
+NVIM_VERSION=0.7.2
 mkdir -p ~/Download/manually; cd ~/Download/manually;
-  curl -fLO https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip;
-  unzip exa-linux-x86_64-v0.10.1.zip -d exa;
+  curl -fLO https://github.com/ogham/exa/releases/download/v${EXA_VERSION}/exa-linux-x86_64-v${EXA_VERSION}.zip;
+  unzip exa-linux-x86_64-v${EXA_VERSION}.zip -d exa;
   sudo cp -f ./exa/bin/exa /usr/bin;
   sudo chown root:root /usr/bin/exa; sudo chmod 755 /usr/bin/exa;
-    curl -fLO https://github.com/sharkdp/hexyl/releases/download/v0.10.0/hexyl_0.10.0_amd64.deb;
-    sudo apt install ./hexyl_0.10.0_amd64.deb -y;
-  curl -fLO https://github.com/sharkdp/bat/releases/download/v0.21.0/bat_0.21.0_amd64.deb;
-  sudo apt install ./bat_0.21.0_amd64.deb -y;
-    curl -fLO https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb;
+    curl -fLO https://github.com/sharkdp/hexyl/releases/download/v${HEXYL_VERSION}/hexyl_${HEXYL_VERSION}_amd64.deb;
+    sudo apt install ./hexyl_${HEXYL_VERSION}_amd64.deb -y;
+  curl -fLO https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat_${BAT_VERSION}_amd64.deb;
+  sudo apt install ./bat_${BAT_VERSION}_amd64.deb -y;
+    curl -fLO https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux64.deb;
     sudo apt install ./nvim-linux64.deb -y;
 rm -rf ./exa;
 
