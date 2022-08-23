e9026d03bf5d96954289014a1fa3c4ed3c47bb27
Creasure <yurxo@bk.ru>
2022-08-23 16:50:39 +0300

    updated and purified most of scripts

--- scripts/iupdate.sh
+++ scripts/iupdate.sh
@@ -1,40 +1,39 @@
-#!/bin/bash
-set -e;
+#!/bin/env bash
+set -e
 
-if [ ! $(pwd | rev | cut -d'/' -f2 | rev) = 'dot-files' ] ||
-    [ ! $(pwd | rev | cut -d'/' -f1 | rev) = 'scripts' ]; then
-  echo "please go to /scripts folder and run script from there!";
-  exit 1;
+if [[ ! $(pwd | rev | cut -d'/' -f2 | rev) = 'dot-files' ]] ||
+   [[ ! $(pwd | rev | cut -d'/' -f1 | rev) = 'scripts' ]]; then
+  echo 'please go to /scripts/ and run script from there!'; exit 1
 else
-  cd $(git rev-parse --show-toplevel); CURRENT_DIR=$(pwd);
-fi;
+  cd $(git rev-parse --show-toplevel); CURRENT_DIR=$(pwd)
+fi
 
-echo "-> /fonts .";
-if [ ! -d ${CURRENT_DIR}/fonts ]; then
-  cd ${CURRENT_DIR};
-  echo "---> cloning /fonts ...";
-  git clone --depth=1 https://github.com/hansumane/fonts.git &> /dev/null;
-  cd ${CURRENT_DIR}/fonts;
-  echo "---> /fonts cloned.";
+echo '-> /fonts/ .'
+if [[ ! -d ${CURRENT_DIR}/fonts/ ]]; then
+  cd ${CURRENT_DIR}/
+  echo '---> cloning /fonts/ ...'
+  git clone --depth=1 https://github.com/hansumane/fonts.git &> /dev/null
+  cd ${CURRENT_DIR}/fonts/
+  echo '---> /fonts/ cloned.'
 else
-  cd ${CURRENT_DIR}/fonts;
-  echo "---> pulling /fonts ...";
-  git pull --rebase &> /dev/null;
-  echo "---> /fonts updated.";
-fi;
+  cd ${CURRENT_DIR}/fonts/
+  echo '---> pulling /fonts/ ...'
+  git pull --rebase &> /dev/null
+  echo '---> /fonts/ updated.'
+fi
 
-echo "-> /wallpapers .";
-if [ ! -d ${CURRENT_DIR}/wallpapers ]; then
-  cd ${CURRENT_DIR};
-  echo "---> cloning /wallpapers ...";
-  git clone --depth=1 https://github.com/hansumane/wallpapers.git &> /dev/null;
-  cd ${CURRENT_DIR}/wallpapers;
-  echo "---> /wallpapers cloned.";
+echo '-> /wallpapers/ .'
+if [[ ! -d ${CURRENT_DIR}/wallpapers/ ]]; then
+  cd ${CURRENT_DIR}/
+  echo '---> cloning /wallpapers/ ...'
+  git clone --depth=1 https://github.com/hansumane/wallpapers.git &> /dev/null
+  cd ${CURRENT_DIR}/wallpapers/
+  echo '---> /wallpapers/ cloned.'
 else
-  cd ${CURRENT_DIR}/wallpapers;
-  echo "---> pulling /wallpapers ...";
-  git pull --rebase &> /dev/null;
-  echo "---> /wallpapers updated.";
-fi;
+  cd ${CURRENT_DIR}/wallpapers/
+  echo '---> pulling /wallpapers/ ...'
+  git pull --rebase &> /dev/null
+  echo '---> /wallpapers/ updated.'
+fi
 
-echo "-> done!";
+echo '-> done!'
