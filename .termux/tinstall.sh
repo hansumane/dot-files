e80a80a591f76c39b4b159450684cbb510f259e9
Creasure <yurxo@bk.ru>
2023-03-16 04:03:41 +0300

    purified termux install script

--- .termux/tinstall.sh
+++ .termux/tinstall.sh
@@ -1,14 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-if [ ! $(pwd | rev | cut -d'/' -f2 | rev) = 'dot-files' ] ||
-    [ ! $(pwd | rev | cut -d'/' -f1 | rev) = '.termux' ]; then
+set -e
+
+if [[ ! $(pwd | rev | cut -d'/' -f2 | rev) = 'dot-files' ]] ||
+   [[ ! $(pwd | rev | cut -d'/' -f1 | rev) = '.termux' ]]; then
   echo "please go to /.termux folder and run script from there!";
   exit 1;
 else
   cd $(git rev-parse --show-toplevel); CURRENT_DIR=$(pwd);
 fi;
 
-rm -f ~/../usr/etc/motd.sh;
 pkg install -y \
   curl git tar zip unzip gzip bzip2 xz-utils \
   neovim zsh exa bat hexyl calc tmux gnupg \
@@ -23,6 +24,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git;
 ln -sf $(which zsh) ~/.termux/shell;
 
 mkdir -p ~/.local/bin ~/.config/nvim;
+rm -f ~/../usr/etc/motd.sh;
 cp -f ${CURRENT_DIR}/.termux/.vimrc ~/.config/nvim/init.vim;
 cp -f ${CURRENT_DIR}/.termux/.zshrc ~;
 cp -f ${CURRENT_DIR}/.termux/font.ttf ~/.termux;
