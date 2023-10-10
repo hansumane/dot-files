b0b3ca5815ba6859e1a00605c44165276e96b430
Creasure <yurxo@bk.ru>
2023-10-10 11:50:02 +0300

    added wlogout and swaylock configs

--- scripts/setup/arch/d-arch-hyprland-setup.sh
+++ scripts/setup/arch/d-arch-hyprland-setup.sh
@@ -15,10 +15,10 @@ fi
 
 yay -Sy --needed \
   gtk2 gtk3 gtk4 qt5-wayland qt6-wayland gtk-engine-murrine \
-  dunst nwg-look-bin hyprland wofi hyprpaper waybar xdg-desktop-portal-hyprland \
+  wlogout swaylock-effect dunst nwg-look-bin hyprland wofi hyprpaper waybar \
+  xdg-desktop-portal-hyprland gnome-themes-extra xdg-user-dirs \
   grim slurp lxappearance pavucontrol dolphin kitty alacritty \
   nm-connection-editor network-manager-applet networkmanager-openvpn \
-  gnome-themes-extra xdg-desktop-portal-gtk xdg-user-dirs \
   flatpak helvum zathura zathura-pdf-mupdf zathura-djvu \
   libayatana-appindicator libappindicator-gtk3 libappindicator-gtk2 \
   firefox firefox-i18n-ru;
