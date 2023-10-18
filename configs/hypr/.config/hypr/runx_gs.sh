557f5f40e85af0d626277977315f8dd63f5a279b
Creasure <yurxo@bk.ru>
2023-10-18 16:03:48 +0300

    returned xdg-desktop-portal-wlr to killall

--- configs/hypr/.config/hypr/runx_gs.sh
+++ configs/hypr/.config/hypr/runx_gs.sh
@@ -5,6 +5,7 @@
 # (already fixed in git: d8daa2a)
 sleep 1
 killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
