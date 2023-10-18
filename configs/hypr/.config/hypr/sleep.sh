7b7a6adfcb3a75cba8e181c8df4b09eee1ec6bb8
Creasure <yurxo@bk.ru>
2023-10-18 16:04:20 +0300

    added color option for compatibility with regular swaylock (not swaylock-effects)

--- configs/hypr/.config/hypr/sleep.sh
+++ configs/hypr/.config/hypr/sleep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 killall swayidle
 swayidle -w \
-  timeout 295 'swaylock -f' \
+  timeout 295 'swaylock -f -c "#1e1e2e"' \
   timeout 300 'hyprctl dispatch dpms off' \
   resume 'hyprctl dispatch dpms on' &
 # before-sleep 'swaylock -f'
