160ce6326557325af72c242f8faf5364b1722e25
Creasure <yurxo@bk.ru>
2023-10-23 18:19:25 +0300

    removed outdated comment from run xdgs script for hyprland

--- configs/hypr/.config/hypr/runx_gs.sh
+++ configs/hypr/.config/hypr/runx_gs.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# runx_gs.sh = run XDGs, this script
-# is required because of a bug in
-# xdg-desktop-portal-hyprland
-# (already fixed in git: d8daa2a)
 sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
