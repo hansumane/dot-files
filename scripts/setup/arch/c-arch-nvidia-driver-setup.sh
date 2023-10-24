d5b5ea72fca0a749f51e95738aad02e27907128b
Creasure <yurxo@bk.ru>
2023-10-24 21:51:04 +0300

    fixed nvidia arch setup script

--- scripts/setup/arch/c-arch-nvidia-driver-setup.sh
+++ scripts/setup/arch/c-arch-nvidia-driver-setup.sh
@@ -5,10 +5,8 @@ set -e
 sudo pacman -Sy --needed \
   mesa nvidia-open-dkms nvidia-utils opencl-nvidia
 
-echo "GRUB_CMDLINE_LINUX_DEFAULT='... nvidia nvidia_modeset nvidia_uvm nvidia_drm nvidia-drm.modeset=1'" | sudo tee -a /etc/default/grub-test
-EDITOR=nvim sudoedit /etc/default/grub-test
+echo "GRUB_CMDLINE_LINUX_DEFAULT='... nvidia nvidia_modeset nvidia_uvm nvidia_drm nvidia-drm.modeset=1'" | sudo tee -a /etc/default/grub
+EDITOR=nvim sudoedit /etc/default/grub
 
+sudo mkdir -p /etc/modprobe.d
 echo "options nvidia-drm modeset=1" | sudo tee /etc/modprobe.d/nvidia.conf
-
-sudo mkinitcpio -P
-sudo update-grub
