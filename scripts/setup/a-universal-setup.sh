dbfd6a694f16b8eb6f5073254a5019acbcfa4e13
Creasure <yurxo@bk.ru>
2024-04-25 14:43:27 +0300

    a-universal-setup-script: create user directories based on OS_TYPE

--- scripts/setup/a-universal-setup.sh
+++ scripts/setup/a-universal-setup.sh
@@ -11,32 +11,51 @@ else
   CURRENT_DIR=$(pwd)
 fi
 
+# install oh-my-zsh
 cd; sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install plugins for oh-my-zsh
 mkdir -p ~/.oh-my-zsh/custom/plugins; cd ~/.oh-my-zsh/custom/plugins
 git clone --depth=1 --recursive https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone --depth=1 --recursive https://github.com/zsh-users/zsh-autosuggestions.git
 
+# install themes for oh-my-zsh
 mkdir -p ~/.oh-my-zsh/custom/themes; cd ~/.oh-my-zsh/custom/themes
-cp -f ${CURRENT_DIR}/themes/zsh_themes/* .
 git clone --depth=1 --recursive https://github.com/romkatv/powerlevel10k.git
+cp -f ${CURRENT_DIR}/themes/zsh_themes/* .
 
-mkdir -p ~/.local/bin
+# install configs
+mkdir -p ~/.config ~/.local/bin
 cp -f ${CURRENT_DIR}/scripts/newup.sh ~
 cp -f ${CURRENT_DIR}/configs/universal/.zshrc ~
 cp -f ${CURRENT_DIR}/configs/universal/.gitconfig ~
 cp -f ${CURRENT_DIR}/configs/universal/.tmux.conf ~
 cp -rf ${CURRENT_DIR}/configs/universal/.config/doom ~/.config
 
-mkdir -p ~/Desktop ~/Downloads ~/Others; cd ~/Others
-mkdir -p etc Coding Documents Music Pictures Shared Templates Videos
-echo 'XDG_DESKTOP_DIR="$HOME/Desktop"
-XDG_DOWNLOAD_DIR="$HOME/Downloads"
-XDG_TEMPLATES_DIR="$HOME/Others/Templates"
-XDG_PUBLICSHARE_DIR="$HOME/Others/Shared"
-XDG_DOCUMENTS_DIR="$HOME/Others/Documents"
-XDG_MUSIC_DIR="$HOME/Others/Music"
-XDG_PICTURES_DIR="$HOME/Others/Pictures"
-XDG_VIDEOS_DIR="$HOME/Others/Videos"' > ~/.config/user-dirs.dirs
-
-# tpm (tmux plugin manager; prefix + I - install; prefix + U - update)
+# install tpm, tmux plugin manager (prefix + I (install) | prefix + U (update))
 git clone --depth=1 --recursive https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+case $OSTYPE in
+  *linux-gnu*)
+    mkdir -p ~/Desktop ~/Downloads ~/Others; cd ~/Others
+    mkdir -p etc Coding Documents Music Pictures Shared Templates Videos
+
+    printf '%s' \
+      'XDG_DESKTOP_DIR="$HOME/Desktop"' $'\n' \
+      'XDG_DOWNLOAD_DIR="$HOME/Downloads"' $'\n' \
+      'XDG_TEMPLATES_DIR="$HOME/Others/Templates"' $'\n' \
+      'XDG_PUBLICSHARE_DIR="$HOME/Others/Shared"' $'\n' \
+      'XDG_DOCUMENTS_DIR="$HOME/Others/Documents"' $'\n' \
+      'XDG_MUSIC_DIR="$HOME/Others/Music"' $'\n' \
+      'XDG_PICTURES_DIR="$HOME/Others/Pictures"' $'\n' \
+      'XDG_VIDEOS_DIR="$HOME/Others/Videos"' $'\n' > ~/.config/user-dirs.dirs
+    ;;
+
+  *linux-android*)
+    mkdir -p ~/Downloads ~/Others/Coding ~/Others/Documents
+    ;;
+
+  *darwin*)
+    mkdir -p ~/Others/Coding
+    ;;
+esac
