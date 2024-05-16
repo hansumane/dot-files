f995f0be171a939844dfb0955072d80c5dda53d2
Creasure <yurxo@bk.ru>
2024-05-16 15:18:52 +0300

    scripts: cleanup-repo: added option to cleanup only one file

--- scripts/cleanup-repo.sh
+++ scripts/cleanup-repo.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 set -xe
 
-cd 'dot-files'  # repo name
+REPO='dot-files'
+FILENAME='font.ttf'
 
+cd "$REPO"
+
+## remove all files but those that present:
 git ls-files > ../to-keep.txt
 git filter-branch --force --index-filter \
   "git rm --ignore-unmatch --cached -qr . ; \
    cat $PWD/../to-keep.txt | tr '\n' '\0' | xargs -d '\0' git reset -q \$GIT_COMMIT --" \
   --prune-empty --tag-name-filter cat -- --all
 
+## remove exactly specified file:
+# git filter-branch --force --prune-empty --index-filter \
+#   "git rm -f --cached --ignore-unmatch '$FILENAME'" \
+#   --tag-name-filter cat -- --all
+
+## shrink the repository:
 rm -rf .git/refs/original
 git reflog expire --expire=now --all
 git gc --aggressive --prune=now
