e876c956b43bc06904a87239ef908e39569cef67
Creasure <yurxo@bk.ru>
2024-05-18 05:29:51 +0300

    scripts/setup: lunarvim: updated to next release

--- scripts/setup/x-lunarvim.sh
+++ scripts/setup/x-lunarvim.sh
@@ -27,5 +27,5 @@ if [[ "$LVIM_NIGHTLY" = "y" ]]; then
   bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 else
   echo "Installing LunarVim release"
-  LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+  LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
 fi
