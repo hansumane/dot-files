e1dc3427eb2e5c451528d746f9d92807a444e834
Creasure <yurxo@bk.ru>
2025-01-28 12:23:38 +0300

    nvchad: updated cscope and its config

--- configs/universal/.config/nvchad/lua/autocmds.lua
+++ configs/universal/.config/nvchad/lua/autocmds.lua
@@ -14,7 +14,7 @@ autocmd("VimEnter", {
       command! M2 :lua SetIndent({ spaces = 2, tabs = 4, noexpand = true })
       command! M4 :lua SetIndent({ spaces = 4, tabs = 8, noexpand = true })
       command! MG :lua SetIndent({ spaces = 2, tabs = 8, noexpand = true })
-      Cscope db add /path/to/cscope.out:/path/to
+      "Cs db add /path/to/cscope.out::@
     ]]
     SetNumber(true)
   end
