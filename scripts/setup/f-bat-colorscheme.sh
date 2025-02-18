e473423ec08c28d8af2926de8c075204f7e4e1a4
Creasure <yurxo@bk.ru>
2025-02-18 17:04:41 +0300

    scripts/setup: updated bat and btop setup scripts
    added user choice for bat colorscheme

--- scripts/setup/f-bat-colorscheme.sh
+++ scripts/setup/f-bat-colorscheme.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 set -e
 
+PROMPT="\
+Choose theme [1]:
+1. Catppuccin Mocha
+"
+
 if command -v bat &> /dev/null; then
 
   DIR="$(bat --config-dir)"
   FILE="$(bat --config-file)"
 
-  rm -rf $DIR
-  mkdir -p $DIR/themes
-  cd $DIR/themes
-  curl -fL \
-    https://raw.githubusercontent.com/catppuccin/bat/main/themes/Catppuccin%20Mocha.tmTheme \
-    -o catppuccin-mocha.tmTheme
-  echo '--theme="catppuccin-mocha"' > $FILE
+  rm -rf "$DIR"
+  mkdir -p "$DIR"/themes
+  cd "$DIR"/themes
+
+  read -p "$PROMPT" CHOICE
+  CHOICE=${CHOICE:-1}
+
+  case "$CHOICE" in
+    *1*)
+      THEME="catppuccin-mocha"
+      curl -fL \
+        https://raw.githubusercontent.com/catppuccin/bat/main/themes/Catppuccin%20Mocha.tmTheme \
+        -o "$THEME".tmTheme
+      ;;
+  esac
+
+  echo "--theme=\"$THEME\"" > "$FILE"
   bat cache --build
 
 fi
--- scripts/setup/f-btop-colorscheme.sh
+++ scripts/setup/f-btop-colorscheme.sh
@@ -3,14 +3,18 @@ set -e
 
 if command -v btop &> /dev/null; then
 
-  DIR=$HOME/.config/btop
+  DIR="$HOME"/.config/btop
 
-  rm -rf $DIR
-  mkdir -p $DIR/temp $DIR/themes
-  cd $DIR/temp
+  rm -rf "$DIR"
+  mkdir -p "$DIR"/temp "$DIR"/themes
+  cd "$DIR"/temp
 
-  git clone --depth=1 https://github.com/catppuccin/btop.git
-  mv $DIR/temp/btop/themes/* $DIR/themes
+  git clone --depth=1 --recursive https://github.com/catppuccin/btop.git
+  mv "$DIR"/temp/btop/themes/* "$DIR"/themes
+  rm -rf "$DIR"/temp/btop
+
+  git clone --depth=1 --recursive https://github.com/rose-pine/btop.git
+  mv "$DIR"/temp/btop/*.theme "$DIR"/themes
   cd && rm -rf $DIR/temp
 
   btop
