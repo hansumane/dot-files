1d12d031c4a0ab74359f93bf0da1a93f225952f0
Creasure <yurxo@bk.ru>
2025-04-21 14:00:31 +0300

    scripts/setup: added script to perform DE clean before switching to another DE or WM

---
+++ scripts/setup/z-de-clean.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+COLOR_RED=$'\e[31m'
+COLOR_GREEN=$'\e[1;32m'
+COLOR_RESET=$'\e[0m'
+
+DE_CLEAN_CACHE_IGNORE="bat chromium clangd emacs flatpak lvim mozilla nvim pip p10k-*"
+DE_CLEAN_CONFIG_IGNORE="alacritty bat btop chromium Code doom emacs eza GitKraken lvim\
+                        nvim user-dirs* *programmer"
+DE_CLEAN_LOCAL_SHARE_IGNORE="containers doom flatpak icons lunarvim lvim nvim *programmer"
+DE_CLEAN_LOCAL_STATE_IGNORE="lvim nvim *programmer"
+
+clean_dir () {
+  dir="$1"
+  ignore="$2"
+
+  cd "$dir"
+  for node in $(find . -maxdepth 1 | sort) ; do
+    bn="$(basename "$node")"
+    fn="$(readlink -f "$node")"
+    skip=0
+
+    for i in $ignore ; do
+      case "$bn" in
+        .|$i) skip=1 ;;
+      esac
+    done
+
+    if [ $skip -ne 1 ] ; then
+      echo "${COLOR_RED}deleting [$bn] $fn...${COLOR_RESET}"
+      # sudo rm -rf "$fn"
+    else
+      echo "skipping [$bn] $fn"
+    fi
+  done
+}
+
+clean_dir ~/.cache "$DE_CLEAN_CACHE_IGNORE"
+clean_dir ~/.config "$DE_CLEAN_CONFIG_IGNORE"
+clean_dir ~/.local/share "$DE_CLEAN_LOCAL_SHARE_IGNORE"
+clean_dir ~/.local/state "$DE_CLEAN_LOCAL_STATE_IGNORE"
