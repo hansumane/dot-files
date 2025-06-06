061b22c5819b7ebf0c7d2884ed21354b57a568b8
Creasure <yurxo@bk.ru>
2025-06-06 22:38:40 +0300

    nvim: new config

--- configs/universal/.config/nvim/init.lua
+++ configs/universal/.config/nvim/init.lua
@@ -1,2 +1,8 @@
-require('basic')
-require('plugins')
+require('defaults').setup()
+require('mappings').setup()
+require('autocmds').setup()
+
+local plugins = require 'plugins'
+plugins.setup()
+
+require('lazy').setup({ plugins.settings }, plugins.lazy_settings)
