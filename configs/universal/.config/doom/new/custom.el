d991ac97d6fde31deef7bdef8ff5be7b787e8555
Creasure <yurxo@bk.ru>
2025-09-01 01:40:51 +0300

    doom: new config

---
+++ configs/universal/.config/doom/new/custom.el
@@ -0,0 +1,5 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ '(org-safe-remote-resources
+  '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'")))
+(custom-set-faces)
