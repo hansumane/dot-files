216edfdbe35877ff106932beaf37845419f7683c
Creasure <yurxo@bk.ru>
2026-07-23 14:54:53 +0300

    scripts/devel/scan-includes: align things

--- scripts/devel/scan-includes.py
+++ scripts/devel/scan-includes.py
@@ -25,13 +25,13 @@ def main(args: list[str]):
     with open(compile_commands_path, "r") as f:
         data: list[dict[str, str]] = json.load(f)
 
-    others: set[str] | list[str] = set()
-    includes: set[str] | list[str] = set()
+    others:        set[str] | list[str] = set()
+    includes:      set[str] | list[str] = set()
     auto_includes: set[str] | list[str] = set()
-    functs: set[str] | list[str] = set()
-    ms: set[str] | list[str] = set()
-    warns: set[str] | list[str] = set()
-    defines: set[str] | list[str] = set()
+    functs:        set[str] | list[str] = set()
+    ms:            set[str] | list[str] = set()
+    warns:         set[str] | list[str] = set()
+    defines:       set[str] | list[str] = set()
 
     for e in data:
         process = len(filters) == 0
@@ -124,22 +124,22 @@ def main(args: list[str]):
         else:
             return 0
 
-    others = sorted(list(others))
-    includes = sorted(list(includes))
+    others        = sorted(list(others))
+    includes      = sorted(list(includes))
     auto_includes = sorted(list(auto_includes))
-    defines = sorted(list(defines))
-    functs = sorted(list(functs))
-    ms = sorted(list(ms))
-    warns = sorted(list(warns))
-    warns = sorted(list(warns), key=warns_sort_key)
+    defines       = sorted(list(defines))
+    functs        = sorted(list(functs))
+    ms            = sorted(list(ms))
+    warns         = sorted(list(warns))
+    warns         = sorted(list(warns), key=warns_sort_key)
 
-    print(*others, "", sep=linesep)
-    print(*includes, "", sep=linesep)
+    print(*others,        "", sep=linesep)
+    print(*includes,      "", sep=linesep)
     print(*auto_includes, "", sep=linesep)
-    print(*defines, "", sep=linesep)
-    print(*functs, "", sep=linesep)
-    print(*ms, "", sep=linesep)
-    print(*warns, sep=linesep)
+    print(*defines,       "", sep=linesep)
+    print(*functs,        "", sep=linesep)
+    print(*ms,            "", sep=linesep)
+    print(*warns,             sep=linesep)
 
 
 if __name__ == "__main__":
